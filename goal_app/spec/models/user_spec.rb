1736df668b8ec2e58c5dfd4e69edb1e18dd1b329
jli57 <jingna13@gmail.com>
2018-09-14 11:43:58 -0700

    Add specs for User

--- goal_app/spec/models/user_spec.rb
+++ goal_app/spec/models/user_spec.rb
@@ -10,9 +10,52 @@ RSpec.describe User, type: :model do
 
   subject(:user) { FactoryBot.build(:user) }
 
-  describe "session_token" do
+  describe "#session_token" do
     it "assigns a session_token when one is not given" do
       expect(user.session_token).not_to be_nil
     end
   end
+
+  describe "password encryption" do
+    it "does not save password to database" do
+      user = FactoryBot.create(:user)
+      user_from_db = User.find_by(username: user.username)
+      expect(user_from_db.password).to be_nil
+    end
+
+    it "uses BCrypt" do
+      expect(BCrypt::Password).to receive(:create).with('password')
+      FactoryBot.build(:user)
+    end
+  end
+
+  describe "#reset_session_token!" do
+    it "changes the session token" do
+      expect{ user.reset_session_token! }.to(change{ user.session_token })
+    end
+  end
+
+  describe "::find_by_credentials" do
+    context "when given valid credentials" do
+      it "returns the user" do
+        user = FactoryBot.create(:user, username: "testtest")
+        expect(
+          User.find_by_credentials(username: "testtest", password: "password")
+        ).to eq(user)
+      end
+    end
+
+    context "when given invalid credentials" do
+      it "returns nil" do
+        user = FactoryBot.create(:user, username: "testtest")
+        expect(
+          User.find_by_credentials(username: "test", password: "password")
+        ).to be_nil
+        expect(
+          User.find_by_credentials(username: "testtest", password: "abcdef")
+        ).to be_nil
+      end
+    end
+  end
+
 end
