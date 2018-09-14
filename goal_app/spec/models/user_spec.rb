6babad2581b94cb11f7ef79cfca4cc84cf899e1e
jli57 <jingna13@gmail.com>
2018-09-14 11:54:07 -0700

    Write User model

--- goal_app/spec/models/user_spec.rb
+++ goal_app/spec/models/user_spec.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint(8)        not null, primary key
+#  username        :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
@@ -39,21 +51,15 @@ RSpec.describe User, type: :model do
     context "when given valid credentials" do
       it "returns the user" do
         user = FactoryBot.create(:user, username: "testtest")
-        expect(
-          User.find_by_credentials(username: "testtest", password: "password")
-        ).to eq(user)
+        expect(User.find_by_credentials("testtest", "password")).to eq(user)
       end
     end
 
     context "when given invalid credentials" do
       it "returns nil" do
         user = FactoryBot.create(:user, username: "testtest")
-        expect(
-          User.find_by_credentials(username: "test", password: "password")
-        ).to be_nil
-        expect(
-          User.find_by_credentials(username: "testtest", password: "abcdef")
-        ).to be_nil
+        expect(User.find_by_credentials("test", "password")).to be_nil
+        expect(User.find_by_credentials("testtest", "abcdef")).to be_nil
       end
     end
   end
