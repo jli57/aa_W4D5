e5ad2b03c30ff87176ad96b0900c6c614eec0ac5
jli57 <jingna13@gmail.com>
2018-09-14 11:17:52 -0700

    Configure factory bot

--- goal_app/config/application.rb
+++ goal_app/config/application.rb
@@ -13,7 +13,7 @@ module GoalApp
 
     config.generators do |g|
       g.test_framework :rspec,
-        :fixtures => false,
+        :fixtures => true,
         :view_specs => false,
         :helper_specs => false,
         :routing_specs => false,
